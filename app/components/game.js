7eabdd0e5f9dbfc99e243b4f344e3f5bdef1e2b2
Viet Truong <viettruong2194@gmail.com>
2016-09-09 23:43:45 -0700

    Uninstall focuseable component and handle input using window event listeners

--- app/components/game.js
+++ app/components/game.js
@@ -1,22 +1,35 @@
 import Ember from 'ember';
-import FocusableComponent from 'ember-component-focus/mixins/focusable-component';
 
 const { Component, inject: { service } } = Ember;
 
-export default Component.extend(FocusableComponent, {
+export default Component.extend({
   classNames: ['game'],
 
   score: service(),
 
-  actions: {
-    focusComponent() {
-      this.focus();
-    },
+  init() {
+    this._super(...arguments);
+    this.addEventListeners();
   },
 
-  keyDown(event) {
-    if (event.key === ' ') {
-      this.get('score').addToScore(1);
+  willDestroy() {
+    this._super(...arguments);
+    this.removeEventListeners();
+  },
+
+  addEventListeners() {
+    window.addEventListener('keydown', this.handleKeyPress.bind(this));
+  },
+
+  removeEventListeners() {
+    window.removeEventListener('keydown', this.handleKeyPress.bind(this));
+  },
+
+  handleKeyPress(event) {
+    switch (event.key) {
+      case ' ':
+        this.get('score').addToScore(1);
+        break;
     }
   },
 });
